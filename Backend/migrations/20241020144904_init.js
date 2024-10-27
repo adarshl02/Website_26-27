649b9e4dea4495d9a4485026f210a4d90ccfc288
[Eklavya Singh] <[eklavyasinghparihar7875@gmail.com]>
2024-10-28 02:59:51 +0530

    added migration for volunteers

--- Backend/migrations/20241020144904_init.js
+++ Backend/migrations/20241020144904_init.js
@@ -2,14 +2,27 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+    return knex.schema.createTable("volunteers", (table) => {
+        table.increments("volunteer_id").primary();
+        table.string('email').notNullable()
+        table.string('phone').notNullable()
+        table.string("enrollment").notNullable().unique();
+        table.string('branch').notNullable()
+        table.integer('batch').notNullable()
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+    })
   
-};
+      
+    
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function (knex) {
+    return knex.schema.dropTable('volunteers')
+      
+  };
+  
