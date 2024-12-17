3eb80a5d89ce733ccedf2cb702527ffd0e076670
[Eklavya Singh] <[eklavyasinghparihar7875@gmail.com]>
2024-12-17 22:48:30 +0530

    fix-some-bugs

--- Backend/routes/art.routes.js
+++ Backend/routes/art.routes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const multer = require("multer");
 const { artCommunity } = require("../controllers/art.controllers.js");
+const { verifyToken } = require("../utils/verifyUser.js");
+const { verify } = require("crypto");
 
 const router = express.Router();
 
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post("/art-community", upload.single("image"), artCommunity);
+router.post(
+  "/art-community",
+  verifyToken,
+  upload.single("image"),
+  artCommunity
+);
 
 module.exports = router;
