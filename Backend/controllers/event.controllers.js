75b7e5621e35ba44530dc653cb87b0f6c369add2
[Eklavya Singh] <[eklavyasinghparihar7875@gmail.com]>
2025-02-14 20:42:26 +0530

    fix-some-bugs

--- Backend/controllers/event.controllers.js
+++ Backend/controllers/event.controllers.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_secret: process.env.RAZORPAY_KEY_SECRET ,
 });
 
 const getEvents = async (req, res) => {
