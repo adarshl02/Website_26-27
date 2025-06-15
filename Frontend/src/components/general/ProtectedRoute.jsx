7391ee511de68b826a2a3259e2b5b83cb1ef0bb4
pratibimbsgsits <clubpratibimb.sgsits@gmail.com>
2025-06-15 13:16:46 +0530

    only backend

--- Frontend/src/components/general/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-
-export const ProtectedRoute = ({ children }) => {
-    const {currentUser} = useSelector((state) => state.user);
-
-    return currentUser ? <Navigate to="/" /> : children;
-};
-
